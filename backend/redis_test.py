580676143b61e8c92da35cbbc4dd8a8feb316971
htamlive <nghichmaychoi@gmail.com>
2023-03-01 11:01:45 +0700

    finish testing

--- backend/redis_test.py
+++ backend/redis_test.py
@@ -15,9 +15,9 @@ INDEX_NAME = "VNU-HCMUS-free-db"
 
 n_vec = 10000
 dim = 1536
-dim = 20
+dim = 384
 
-vector_field_name = "v"
+vector_field_name = "vector"
 rating_field_name = "rating"
 
 k = 10
@@ -33,7 +33,7 @@ def delete_data(client: Redis):
 
 
 def create_index():
-    schema = (VectorField(vector_field_name, "HNSW", {"TYPE": "FLOAT32", "DIM": dim, "DISTANCE_METRIC": "L2"}))
+    schema = (VectorField(vector_field_name, "HNSW", {"TYPE": "FLOAT32", "DIM": dim, "DISTANCE_METRIC": "COSINE"}))
     r.ft(INDEX_NAME).create_index(schema)
 
 # Define a function to generate GPT-3 embeddings
@@ -50,11 +50,8 @@ def add_to_db(key : str, text : str):
 def test_input():
 
     for i, document in enumerate(documents):
-        key =f'Hello_{i}'
+        key =f'{i}'
         add_to_db(key, document)
-
-        
-
 
 #%%
 
@@ -78,8 +75,12 @@ create_index()
 test_input()
 # %%
 
+data = search("man's friend",2)
 
-search("The quick brown fox jumps over the lazy dog.").docs[0].id
+ids = [int(item.id) for item in data.docs]
+
+[print(documents[id]) for id in ids]
+
 # Output: ['The lazy dog slept in the sun.', 'The quick brown fox jumps over the lazy dog.', 'The quick brown fox ate the lazy dog.']
 
 #print(search("man's best friend"))
@@ -88,7 +89,7 @@ search("The quick brown fox jumps over the lazy dog.").docs[0].id
 
 #%%
 
-print("Index size: ", r.ft(INDEX_NAME).info()['num_docs'])
+# print("Index size: ", r.ft(INDEX_NAME).info()['num_docs'])
 
 
 
@@ -96,19 +97,19 @@ print("Index size: ", r.ft(INDEX_NAME).info()['num_docs'])
 #%%
 
 
-def load_data(client : Redis, n, d):
-    for i in range(1, n+1):
-        np_vector = np.random.rand(1, d).astype(np.float32)
-        client.hset(i, mapping = {vector_field_name: np_vector.tobytes(),
-                                    rating_field_name: 10*(i/n)})  # ratings ranges from 0-10, proportional to the id)
+# def load_data(client : Redis, n, d):
+#     for i in range(1, n+1):
+#         np_vector = np.random.rand(1, d).astype(np.float32)
+#         client.hset(i, mapping = {vector_field_name: np_vector.tobytes(),
+#                                     rating_field_name: 10*(i/n)})  # ratings ranges from 0-10, proportional to the id)
         
 
     
-def print_results(res):
-    docs = [int(doc.id) for doc in res.docs]
-    dists = [float(doc.dist) if hasattr(doc, 'dist') else '-' for doc in res.docs]
-    print(f"got {len(docs)} doc ids: ", docs)
-    print("\ndistances: ", dists)
+# def print_results(res):
+#     docs = [int(doc.id) for doc in res.docs]
+#     dists = [float(doc.dist) if hasattr(doc, 'dist') else '-' for doc in res.docs]
+#     print(f"got {len(docs)} doc ids: ", docs)
+#     print("\ndistances: ", dists)
 
 
 
@@ -119,41 +120,41 @@ def print_results(res):
 # r.ft().config_set("default_dialect", 2)
 
 # load vectors with meta-data
-np.random.seed(42)
-load_data(r, n_vec, dim)
+# np.random.seed(42)
+# load_data(r, n_vec, dim)
 
-print("Index size: ", r.ft().info()['num_docs'])
+# print("Index size: ", r.ft().info()['num_docs'])
 
-query_vector = np.random.rand(1, dim).astype(np.float32)  # This is my query vector
+# query_vector = np.random.rand(1, dim).astype(np.float32)  # This is my query vector
 
-#%%
+# #%%
 
 
-# Give me the top 10 docs with vectors similar to mine
-query_vector = np.random.rand(1, dim).astype(np.float32)
-q = Query(f'*=>[KNN 10 @{vector_field_name} $vec_param]=>{{$yield_distance_as: dist}}').sort_by(f'dist')
-res = r.ft().search(q, query_params = {'vec_param': query_vector.tobytes()})
+# # Give me the top 10 docs with vectors similar to mine
+# query_vector = np.random.rand(1, dim).astype(np.float32)
+# q = Query(f'*=>[KNN 10 @{vector_field_name} $vec_param]=>{{$yield_distance_as: dist}}').sort_by(f'dist')
+# res = r.ft().search(q, query_params = {'vec_param': query_vector.tobytes()})
 
-print_results(res)
 # print_results(res)
+# # print_results(res)
 
 
-# %%
-delete_data(r)
-# %%
-schema = (VectorField("v", "HNSW", {"TYPE": "FLOAT32", "DIM": dim, "DISTANCE_METRIC": "COSINE"}),)
-r.ft().create_index(schema)
+# # %%
+# delete_data(r)
+# # %%
+# schema = (VectorField("v", "HNSW", {"TYPE": "FLOAT32", "DIM": dim, "DISTANCE_METRIC": "COSINE"}),)
+# r.ft().create_index(schema)
 
-r.hset("a", mapping= {"v": np.array(get_embedding(text=documents[0])[:dim], dtype=np.float32).tobytes()})
-r.hset("b", mapping= {"v": np.array(get_embedding(text=documents[1])[:dim], dtype=np.float32).tobytes()})
-r.hset("c", mapping= {"v": np.array(get_embedding(text=documents[2])[:dim], dtype=np.float32).tobytes()})
-#%%
-q = Query("*=>[KNN 2 @v $vec]").return_field("__v_score").dialect(2)
-r.ft("idx").search(q, query_params={"vec": np.array(get_embedding(text=documents[0])[:dim], dtype=np.float32).tobytes()})
-# %%
+# r.hset("a", mapping= {"v": np.array(get_embedding(text=documents[0])[:dim], dtype=np.float32).tobytes()})
+# r.hset("b", mapping= {"v": np.array(get_embedding(text=documents[1])[:dim], dtype=np.float32).tobytes()})
+# r.hset("c", mapping= {"v": np.array(get_embedding(text=documents[2])[:dim], dtype=np.float32).tobytes()})
+# #%%
+# q = Query("*=>[KNN 2 @v $vec]").return_field("__v_score").dialect(2)
+# r.ft("idx").search(q, query_params={"vec": np.array(get_embedding(text=documents[0])[:dim], dtype=np.float32).tobytes()})
+# # %%
 
 
-print(len(get_embedding(text=documents[0])))
+# print(len(get_embedding(text=documents[0])))
 
 # %%
 
