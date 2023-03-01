2b371ca5546303b68f995d2edb675a80298e8edc
chulanpro5 <chulanpro5@gmail.com>
2023-03-01 14:53:54 +0700

    add some note:

--- backend/models.py
+++ backend/models.py
@@ -26,17 +26,36 @@ def split_paragraph(paragraph: str, max_tokens: int = MAX_TOKENS):
     )
     return response.choices[0].text.strip()
 
-def paraphrase_paragraph(paragraph: str, max_tokens: int = MAX_TOKENS) -> str:
-    prompt = "paraphrase the following paragraph by these requirement :\n\n" + paragraph + "\n\n1."
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=TEMPERATURE,
-    )
-    return response.choices[0].text.strip()
+# def paraphrase_paragraph(paragraph: str, max_tokens: int = MAX_TOKENS) -> str:
+#     prompt = "paraphrase the following paragraph by these requirement :\n\n" + paragraph + "\n\n1."
+#     response = openai.Completion.create(
+#         engine="text-davinci-003",
+#         prompt=prompt,
+#         max_tokens=max_tokens,
+#         n=1,
+#         stop=None,
+#         temperature=TEMPERATURE,
+#     )
+#     return response.choices[0].text.strip()
+
+"""
+def paraphrase_paragraph(extracted-topic):
+    - summarize the paragraph: summarize_paragraph()
+    - for each topic in extracted-topic:
+        if topic is not in redis database: skip
+        if topic is in redis database:
+            - searching for the raw text of the topic: search_topic()
+            if the raw text is found:
+                - paraphrase the raw text: paraphrase_text() //cần prompt
+            if the raw text is not found:
+                - generate context for the topic: generate_context() //cần prompt
+                - paraphrase the context: paraphrase_text() 
+
+            - insert the paraphrased text into the paragraph: insert_paragraph()
+    
+    - paraphrase hole paragraph
+    - return the paragraph
+"""
 
 def extract_topic(paragraph: str, max_tokens: int = MAX_TOKENS) -> list:
     prompt = prompt_list["extract_topic"]["prefix"] + paragraph + prompt_list["extract_topic"]["suffix"]
@@ -50,6 +69,14 @@ def extract_topic(paragraph: str, max_tokens: int = MAX_TOKENS) -> list:
     )
     return response.choices[0].text.strip()
 
+"""
+    - Split the document into paragraphs: split_paragraph()
+for each paragraph in the document:
+    - extract the topic of the paragraph: extract_topic()
+    - paraphrase the paragraph: paraphrase_paragraph(extract_topic)
+
+"""
+
 f = open("doc.txt", "r", encoding="utf-8")
 text = f.read()
 f.close()
@@ -57,7 +84,17 @@ f.close()
 paragraphs = split_paragraph(text).split("\n\n")
 # print(paragraphs)
 
-for i in range(len(paragraphs)):
-    paragraphs[i] = paraphrase_paragraph(paragraphs[i])
+# for i in range(len(paragraphs)):
+#     paragraphs[i] = paraphrase_paragraph(paragraphs[i])
 
 print(paragraphs)
+
+
+""" Redis query
+insert_topic(topic) -> 1: success, 0: fail
+query_topic(topic, threshold = 0.9) -> 1: found, 0: not found
+
+insert_paragraph(paragraph) -> 1: success, 0: fail
+query_paragraph(sentence, threshold = 0.9) -> str: paragraph, None: not found
+
+"""
