eb91313387239f238beb5ab1e790ee66b676e7c2
chulanpro5 <chulanpro5@gmail.com>
2023-03-02 17:17:05 +0700

    wrap models into class

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,12 @@
 from flask import Flask,request
-from ModelInteration import ModelInteraction
+from models import ModelInteraction, DocumentInteraction
 import os
 import json
 from flask_cors import CORS
 from dotenv import load_dotenv
+load_dotenv('./key.env.local')
 
-host = "localhost"
-port = 6379
-
-model = ModelInteraction()
-
-model.connect_database(host, port)
+test = DocumentInteraction()
 
 app = Flask(__name__)
 CORS(app)
@@ -20,14 +16,14 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route('/api/predict', methods=['POST'])
-def predict():
-    query = request.json.get("query")
-    query_with_contexts = retrieve_from_gpt(query)
-    print(query_with_contexts)
-    bot = complete(query_with_contexts)
+# @app.route('/api/predict', methods=['POST'])
+# def predict():
+#     query = request.json.get("query")
+#     query_with_contexts = retrieve_from_gpt(query)
+#     print(query_with_contexts)
+#     bot = complete(query_with_contexts)
 
-    return {"bot": bot}
+#     return {"bot": bot}
     
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,171 +1,215 @@
-#%%
+DEBUG = True
+
 import complete_prompt
 
 import os
 import openai
 import json
 from dotenv import load_dotenv
-load_dotenv('./key.env')
+load_dotenv('./key.env.local')
 from RedisDatabase import *
 
-
+# openai api key
+openai.api_key = os.environ["OPENAI_API_KEY"]
 OPENAI_EMBEDDINGS_ENGINE = "text-embedding-ada-002"
 OPENAI_COMPLETIONS_ENGINE = "text-davinci-003"
 MAX_TOKENS = 2000
 TEMPERATURE = 0
 
 # load prompt from json file
-#%%
-redisDatabase = RedisDatabase()
-#%%
 prompt_list =  json.load(open('prompt.json', 'r'))
-openai.api_key = os.environ["OPENAI_API_KEY"]
-#%%
 
-def complete(prompt, max_tokens: int = MAX_TOKENS) -> str:
-    response = openai.Completion.create(
-        engine = OPENAI_COMPLETIONS_ENGINE,
-        prompt = prompt,
-        max_tokens = max_tokens,
-        n = 1,
-        stop = None,
-        temperature = TEMPERATURE,
-    )
-    return response.choices[0].text.strip()
 
-def split_document(paragraph: str) -> list:
-    """
-    Input: paragraph
-    Output: list of paragraph
-    """
-    prompt = prompt_list["split_document"]["prefix"] + paragraph + prompt_list["split_document"]["suffix"]
-    response = json.loads(complete(prompt).replace('\n','')) # remove newline character
-
-    return response
-
-def extract_topic(paragraph: str) -> list:
-    """
-    Input: paragraph
-    Output: list of topic
-    """
-    prompt = prompt_list["extract_topic"]["prefix"] + paragraph + prompt_list["extract_topic"]["suffix"]
-    response = json.loads(complete(prompt))
-
-    return response
-
-def summarize_paragraph(paragraph: str) -> str:
-    """
-    Input: paragraph
-    Output: paragraph
-    """
-    prompt = prompt_list["summarize"]["prefix"] + paragraph + prompt_list["summarize"]["suffix"]
-    response = complete(prompt)
-    return response
-
-def conditonal_summarize_paragraph(topics: list, paragraph: str) -> str:
-    """
-    Input: topics, paragraph
-    Output: paragraph
-    """
-    prompt = complete_prompt.prompt_conditional_summary(topics, paragraph)
-    response = complete(prompt)
-    return response
-
-def expand_topics_from_clue(topics: list, paragraph: str, clue: str) -> str:
-    """
-    Input: topics, paragraph, clue
-    Output: paragraph
-    """
-    prompt = complete_prompt.expand_topics_from_clue(topics, paragraph, clue)
-    response = complete(prompt)
-    return response
-
-def expand_topics_from_GPT(topics: list, paragraph: str) -> str:
-    """
-    Input: topics, paragraph
-    Output: paragraph
-    """
-    prompt = complete_prompt.expand_topics_from_GPT(topics, paragraph)
-    response = complete(prompt)
-    return response
-
-def rephase_document(document: str) -> list:
-    """
-    Input: document
-    Output: list of paragraph
-    """
-    prompt = prompt_list["rephrase_document"]["prefix"] + document + prompt_list["rephrase_document"]["suffix"]
-    response = json.loads(complete(prompt).replace('\n','')) # remove newline character
-
-    return response
-
-def processing_document(document: str) -> list:
-    """
-    Input: document
-    Output: list of list sentences
-    """
-    document = document.replace('\n', ' ') # remove newline character
-    paragraphs = split_document(document)
-    result = ""
-    for paragraph in paragraphs:
-        result += processing_paragraph(paragraph) + ' '
+class ModelInteraction():
+    def __init__(self):
+        pass
     
-    paragraphs = rephase_document(result)
+    def complete(self, prompt, max_tokens: int = MAX_TOKENS) -> str:
+        response = openai.Completion.create(
+            engine = OPENAI_COMPLETIONS_ENGINE,
+            prompt = prompt,
+            max_tokens = max_tokens,
+            n = 1,
+            stop = None,
+            temperature = TEMPERATURE,
+        )
+        return response.choices[0].text.strip()
+
+    def split_document(self, paragraph: str) -> list:
+        """
+        Input: paragraph
+        Output: list of paragraph
+        """
+        if (DEBUG) : print("def split_document(self, paragraph: str) -> list:" + "\n")
+        prompt = prompt_list["split_document"]["prefix"] + paragraph + prompt_list["split_document"]["suffix"]
+        response = json.loads(self.complete(prompt).replace('\n','')) # remove newline character
+
+        return response
+
+    def extract_topic(self, paragraph: str) -> list:
+        """
+        Input: paragraph
+        Output: list of topic
+        """
+        if (DEBUG) : print("def extract_topic(self, paragraph: str) -> list:" + "\n")
+        prompt = prompt_list["extract_topic"]["prefix"] + paragraph + prompt_list["extract_topic"]["suffix"]
+        response = json.loads(self.complete(prompt))
+
+        return response
+
+    def summarize_paragraph(self, paragraph: str) -> str:
+        """
+        Input: paragraph
+        Output: paragraph
+        """
+        if (DEBUG) : print("def summarize_paragraph(self, paragraph: str) -> str:" + "\n")
+        prompt = prompt_list["summarize"]["prefix"] + paragraph + prompt_list["summarize"]["suffix"]
+        response = self.complete(prompt)
+        return response
+
+    def conditonal_summarize_paragraph(self, topics: list, paragraph: str) -> str:
+        """
+        Input: topics, paragraph
+        Output: paragraph
+        """
+        if (DEBUG) : print("def conditonal_summarize_paragraph(self, topics: list, paragraph: str) -> str:" + "\n")
+        prompt = complete_prompt.prompt_conditional_summary(topics, paragraph)
+        response = self.complete(prompt)
+        return response
+
+    def expand_topics_from_clue(self, topics: list, paragraph: str, clue: str) -> str:
+        """
+        Input: topics, paragraph, clue
+        Output: paragraph
+        """
+        if (DEBUG) : print("def expand_topics_from_clue(self, topics: list, paragraph: str, clue: str) -> str:" + "\n")
+        prompt = complete_prompt.expand_topics_from_clue(topics, paragraph, clue)
+        response = self.complete(prompt)
+        return response
+
+    def expand_topics_from_GPT(self, topics: list, paragraph: str) -> str:
+        """
+        Input: topics, paragraph
+        Output: paragraph
+        """
+        if (DEBUG) : print("def expand_topics_from_GPT(self, topics: list, paragraph: str) -> str:" + "\n")
+        prompt = complete_prompt.expand_topics_from_GPT(topics, paragraph)
+        response = self.complete(prompt)
+        return response
+
+    def rephase_document(self, document: str) -> list:
+        """
+        Input: document
+        Output: list of paragraph
+        """
+        if (DEBUG) : print("def rephase_document(self, document: str) -> list:" + "\n")
+        prompt = prompt_list["rephrase_document"]["prefix"] + document + prompt_list["rephrase_document"]["suffix"]
+        response = json.loads(self.complete(prompt).replace('\n','')) # remove newline character
+
+        return response
+
+class DocumentInteraction():
+    def __init__(self):
+        self.data = []
+        self.document = ""
+
+        self.model = ModelInteraction()
+        # self.redis = RedisDatabase()
+
+    def insert_document(self, document: str) -> None:
+        self.document = document
+
+    def get_data(self) -> list:
+        return self.data
     
-    data = []
-    for paragraph in paragraphs:
-        data.append(paragraph_to_sentence(paragraph))
+    def get_document(self) -> str:
+        return self.document
 
-    return data
+    def processing_document(self) -> None:
+        """
+        Input: document
+        Output: list of list sentences
+        """
+        if (DEBUG) : print("def processing_document(self) -> None:" + "\n")
+        self.document = self.document.replace('\n', ' ') # remove newline character
+        paragraphs = self.model.split_document(self.document)
+        result = ""
+        for paragraph in paragraphs:
+            result += self.processing_paragraph(paragraph) + ' '
+        
+        paragraphs = self.model.rephase_document(result)
+        
+        
+        for paragraph in paragraphs:
+            self.data.append(self.paragraph_to_sentence(paragraph))
 
-def processing_paragraph(paragraph: str) -> str:
-    """
-    Input: paragraph
-    Output: paragraph (summarized)
-    """
-    topics = extract_topic(paragraph)
+        return
     
-    for topic in topics:
-        if(RedisDatabase.query_topic(topic) == None):
-            topics.remove(topic)
+    def user_click_sentence(self, sentence: str) -> str:
+        """
+        """
+        for paragraph in self.data:
+            for s in paragraph:
+                if(s == sentence):
+                    new_paragraph = ""
+                    topics = self.model.extract_topic(s)
+                    # check if the sentence is in redis database
+                    query = self.redis.query_sentence(s)
+                    query = None
+                    if(query == None):
+                        new_paragraph =  self.model.expand_topics_from_GPT(topics, paragraph)
+                    else:
+                        new_paragraph =  self.model.expand_topics_from_clue(topics, query, s)
 
-    if(len(topics) == 0):
-        return summarize_paragraph(paragraph)
-    else:
-        return conditonal_summarize_paragraph(topics, paragraph)
+                    new_paragraph = self.paragraph_to_sentence(new_paragraph)
+                    # update the paragraph
+                    paragraph = new_paragraph
+                    # insert each topic to redis database
+                    # for topic in topics:
+                    #     self.redis.insert_topic(topic, paragraph)
+        
+        return 
 
-def paragraph_to_sentence(paragraph: str) -> list:
-    """
-    Input: paragraph
-    Output: list of sentences
-    """
-    sentences = paragraph.split('.')
-    return sentences
+    def processing_paragraph(self, paragraph: str) -> str:
+        """
+        Input: paragraph
+        Output: paragraph (summarized)
+        """
+        topics = self.model.extract_topic(paragraph)
+        
+        # for topic in topics:
+        #     if(self.redis.query_topic(topic) == None):
+        #         topics.remove(topic)
 
-def user_click_sentence(data: list, sentence: str) -> str:
-    """
-    """
-    for paragraph in data:
-        for s in paragraph:
-            if(s == sentence):
-                new_paragraph = ""
-                topics = extract_topic(s)
-                # check if the sentence is in redis database
-                query = RedisDatabase.query_sentence(s)
-                if(query == None):
-                    new_paragraph =  expand_topics_from_GPT(topics, paragraph)
-                else:
-                    new_paragraph =  expand_topics_from_clue(topics, query, s)
+        if(len(topics) == 0):
+            return self.model.summarize_paragraph(paragraph)
+        else:
+            return self.model.conditonal_summarize_paragraph(topics, paragraph)
 
-                new_paragraph = paragraph_to_sentence(new_paragraph)
-                # update the paragraph
-                paragraph = new_paragraph
-                # insert each topic to redis database
-                for topic in topics:
-                    RedisDatabase.insert_topic(topic, paragraph)
+    def paragraph_to_sentence(self, paragraph: str) -> list:
+        """
+        Input: paragraph
+        Output: list of sentences
+        """
+        sentences = paragraph.split('.')
+        # remove empty string
+        sentences = list(filter(None, sentences))
+        return sentences
     
-    return data
 
+if __name__ == "__main__":
+    # load text from text file
+    with open('wiki_1.txt', 'r') as file:
+        text = file.read().replace('\n', '')
+    test = DocumentInteraction()
+    test.insert_document(text)
+    print(test.get_document())
+    test.processing_document()
+    data = json.dumps(test.get_data())
+    # save data to json file
+    with open('data.json', 'w') as file:
+        file.write(data)
+    print(data)
 
 """ Prompt
 1. summarize 1 đoạn có điều kiện
