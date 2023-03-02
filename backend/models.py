d4a4b48cf709cff64604fa6d3f3c37e782372be2
chulanpro5 <chulanpro5@gmail.com>
2023-03-02 11:22:32 +0700

    update docs processing

--- backend/models.py
+++ backend/models.py
@@ -33,58 +33,132 @@ def complete(prompt, max_tokens: int = MAX_TOKENS) -> str:
     return response.choices[0].text.strip()
 
 def split_document(paragraph: str) -> list:
-    # print("slit document")
+    """
+    Input: paragraph
+    Output: list of paragraph
+    """
     prompt = prompt_list["split_document"]["prefix"] + paragraph + prompt_list["split_document"]["suffix"]
     response = json.loads(complete(prompt).replace('\n','')) # remove newline character
 
     return response
 
 def extract_topic(paragraph: str) -> list:
-    # print("extract topic")
+    """
+    Input: paragraph
+    Output: list of topic
+    """
     prompt = prompt_list["extract_topic"]["prefix"] + paragraph + prompt_list["extract_topic"]["suffix"]
     response = json.loads(complete(prompt))
 
     return response
 
 def summarize_paragraph(paragraph: str) -> str:
-    # print("summarize paragraph")
+    """
+    Input: paragraph
+    Output: paragraph
+    """
     prompt = prompt_list["summarize"]["prefix"] + paragraph + prompt_list["summarize"]["suffix"]
     response = complete(prompt)
     return response
 
 def conditonal_summarize_paragraph(topics: list, paragraph: str) -> str:
+    """
+    Input: topics, paragraph
+    Output: paragraph
+    """
     prompt = complete_prompt.prompt_conditional_summary(topics, paragraph)
     response = complete(prompt)
     return response
 
 def expand_topics_from_clue(topics: list, paragraph: str, clue: str) -> str:
+    """
+    Input: topics, paragraph, clue
+    Output: paragraph
+    """
     prompt = complete_prompt.expand_topics_from_clue(topics, paragraph, clue)
     response = complete(prompt)
     return response
 
 def expand_topics_from_GPT(topics: list, paragraph: str) -> str:
+    """
+    Input: topics, paragraph
+    Output: paragraph
+    """
     prompt = complete_prompt.expand_topics_from_GPT(topics, paragraph)
     response = complete(prompt)
     return response
 
-def rephase_document(paragraph: str) -> list:
-    prompt = prompt_list["rephrase_document"]["prefix"] + paragraph + prompt_list["rephrase_document"]["suffix"]
+def rephase_document(document: str) -> list:
+    """
+    Input: document
+    Output: list of paragraph
+    """
+    prompt = prompt_list["rephrase_document"]["prefix"] + document + prompt_list["rephrase_document"]["suffix"]
     response = json.loads(complete(prompt).replace('\n','')) # remove newline character
 
     return response
 
-def processing_document(document: str) -> str:
-    # print("processing document")
+def processing_document(document: str) -> list:
+    """
+    Input: document
+    Output: list of list sentences
+    """
     paragraphs = split_document(document)
     result = ""
     for paragraph in paragraphs:
         result += processing_paragraph(paragraph) + ' '
     
-    merge_paragraphs = rephase_document(result)
+    paragraphs = rephase_document(result)
+    
+    data = []
+    for paragraph in paragraphs:
+        data.append(paragraph_to_sentence(paragraph))
 
+    return data
 
 def processing_paragraph(paragraph: str) -> str:
+    """
+    Input: paragraph
+    Output: paragraph (summarized)
+    """
+    topics = extract_topic(paragraph)
     
+    for topic in topics:
+        if(RedisDatabase.query_topic(topic) == None):
+            topics.remove(topic)
+
+    if(len(topics) == 0):
+        return summarize_paragraph(paragraph)
+    else:
+        return conditonal_summarize_paragraph(topics, paragraph)
+
+def paragraph_to_sentence(paragraph: str) -> list:
+    """
+    Input: paragraph
+    Output: list of sentences
+    """
+    sentences = paragraph.split('.')
+    return sentences
+
+def user_click_sentence(data: list, sentence: str) -> str:
+    """
+    """
+    for paragraph in data:
+        for s in paragraph:
+            if(s == sentence):
+                new_paragraph = ""
+                # check if the sentence is in redis database
+                query = RedisDatabase.query_sentence(s)
+                if(query == None):
+                    new_paragraph =  expand_topics_from_GPT(extract_topic(s), paragraph)
+                else:
+                    new_paragraph =  expand_topics_from_clue(extract_topic(s), query, s)
+
+                new_paragraph = paragraph_to_sentence(new_paragraph)
+                # update the paragraph
+                paragraph = new_paragraph
+    
+    return data
 
 
 """ Prompt
@@ -103,7 +177,6 @@ def processing_paragraph():
         if topic is in redis database:
             summarize_paragraph_prompt_1()
     
-    - paraphrase hole paragraph
     - return the paragraph
 """
 
